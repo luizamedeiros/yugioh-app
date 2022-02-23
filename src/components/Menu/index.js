7c6c053345d10b4ba69f0ace5c23f19eb8a755d5
Luiza Medeiros <luizadmeds@gmail.com>
2022-02-23 14:26:02 -0300

    Menu component ready and integrated

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,22 @@
+import { Link } from 'react-router-dom';
 import './style.css';
 
 const Menu = () => {
     return (
-    <div className="menu">
-        <h1 className="menu-title">Epic Library</h1>
-        <ul className="menu-list">
-            <li>Home</li>
-            <li>Deck Builder</li>
-            <li>My Decks</li>
-            
-        </ul>
-    </div>
+        <div className="menu">
+            <h1 className="menu-title">Epic Library</h1>
+            <ul className="menu-list">
+                <Link to="/">
+                    <li>Home</li>
+                </Link>
+                <Link to="/deck-builder">
+                    <li>Deck Builder</li>
+                </Link>
+                <Link to="/my-decks">
+                    <li>My Decks</li>
+                </Link>
+            </ul>
+        </div>
     );
 }
 

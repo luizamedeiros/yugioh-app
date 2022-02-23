ef7db047146674d288b596a6b2ac2f003461ee1f
Luiza Medeiros <luizadmeds@gmail.com>
2022-02-23 14:38:46 -0300

    Migrating Cards page to ts

--- src/pages/Cards/index.tsx
+++ src/pages/Cards/index.tsx
@@ -1,16 +1,27 @@
 import { useEffect, useState } from 'react';
-import api from '../../../src/services/axios';
-import Card from '../../../src/components/Card';
+import api from '../../services/axios';
+import Card from '../../components/Card';
 import './style.css';
+
+type ICard = {
+  id: string;
+  name: string;
+  attribute: string;
+  card_images: {
+    id: string;
+    image_url: string;
+    image_url_small: string;
+  }[];
+}
 
 const Cards = () => {
 
-  const [cards, setCards] = useState([]);
+  const [cards, setCards] = useState<ICard[]>([]);
   useEffect(() => {
     api.get('cards?_limit=30')
-    .then((response) => {
-      setCards(response.data);
-    })
+      .then((response) => {
+        setCards(response.data);
+      })
       .catch((error) => {
         console.log(error);
       })
@@ -21,6 +32,7 @@ const Cards = () => {
     <div className='container'>
       {cards.map((card) =>
         <Card
+          key={card.id}
           name={card.name}
           attribute={card.attribute}
           cardImage={card.card_images[0].image_url_small}

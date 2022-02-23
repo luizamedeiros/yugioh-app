e871df1ad48c948ac7a8296b75997872c2ace272
Luiza Medeiros <luizadmeds@gmail.com>
2022-02-23 13:50:08 -0300

    Adding new routes to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 import Cards from './pages/Cards';
+import MyDecks from './pages/MyDecks';
+import DeckBuilder from './pages/DeckBuilder';
+import CardDetails from './pages/CardDetails';
 
 const App = () => {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Cards />}>
-        </Route>
+        <Route path="/" element={<Home />}/>
+        <Route path="/cards" element={<Cards />}/>
+        <Route path="/my-decks" element={<MyDecks />}/>
+        <Route path="/card-details" element={<CardDetails />}/>
+        <Route path="/deck-builder" element={<DeckBuilder />}/>
       </Routes>
     </BrowserRouter>
   );

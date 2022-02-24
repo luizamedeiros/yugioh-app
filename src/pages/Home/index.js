5a2b86a1fac5951fd3b05947a7fe9d0b490e6a2e
Luiza Medeiros <luizadmeds@gmail.com>
2022-02-24 16:41:26 -0300

    Structuring home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,15 @@
-const Home = () =>{
-    return <div/>;
+import { MONSTERS_RACE, SPELL_RACES, TRAP_RACES } from '../../utils/types';
+import CardType from '../../components/CardType';
+
+
+const Home = () => {
+    return (
+        <div className='container home'>
+            <CardType title={"Monsters"} info={MONSTERS_RACE}/>
+            <CardType title={"Spells"} info={SPELL_RACES}/>
+            <CardType title={"Traps"} info={TRAP_RACES}/>
+            </div>
+    );
 }
 
 export default Home;

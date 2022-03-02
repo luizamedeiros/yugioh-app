9a44a820402ec1d446a9d5ec14ef3f0324a7b220
Luiza Medeiros <luizadmeds@gmail.com>
2022-03-02 09:21:03 -0300

    Adding icons to home page

--- src/components/CardType/index.tsx
+++ src/components/CardType/index.tsx
@@ -5,13 +5,17 @@ import './style.scss';
 type CardTypeInfo = {
     title: string;
     info: string[];
+    icon: any;
 }
 
-const CardType: React.FC<CardTypeInfo> = ({title, info}) => {
+const CardType: React.FC<CardTypeInfo> = ({ icon, info, title }) => {
     const color = handleCardColor(title);
     return (
         <div className="type-container">
-            <h2>{title}</h2>
+            <div className="type-header">
+                <img src={icon} alt="icon" width={50} />
+                <h2>{title}</h2>
+            </div>
             <div className='type-items'>
                 {
                     info.map(type => (
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,26 @@
 import { MONSTERS_RACE, SPELL_RACES, TRAP_RACES } from '../../utils/types';
+import Dragao from '../../assets/dragao.png';
+import Kuriboh from '../../assets/kuriboh.png';
+import MagoNegro from '../../assets/mago_negro.png';
 import CardType from '../../components/CardType';
 
 
 const Home = () => {
     return (
         <div className='container home'>
-            <CardType title={"Monsters"} info={MONSTERS_RACE}/>
-            <CardType title={"Spells"} info={SPELL_RACES}/>
-            <CardType title={"Traps"} info={TRAP_RACES}/>
+            <CardType 
+            icon={MagoNegro}
+            info={MONSTERS_RACE}
+            title={"Monsters"}/>
+            <CardType 
+            icon={Kuriboh}
+            info={SPELL_RACES}
+            title={"Spells"} 
+            />
+            <CardType 
+            icon={Dragao}
+            info={TRAP_RACES}
+            title={"Traps"}/> 
             </div>
     );
 }

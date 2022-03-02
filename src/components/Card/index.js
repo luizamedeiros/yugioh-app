838d380b2454077a8c9a6a314730e41513e20674
Luiza Medeiros <luizadmeds@gmail.com>
2022-03-02 10:08:03 -0300

    style changes

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,11 +4,10 @@ const Card = ({name, attribute, cardImage}) => {
     return (
         <div className='card-item'>
             <img
-                width={140}
                 alt={name}
                 src={cardImage}
             />
-            <span className='center'>{name}</span>
+            <span className='center bold'>{name}</span>
             <span className='center'>Attribute: {attribute}</span>
         </div>
     )
